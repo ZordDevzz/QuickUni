5937e4f689b5183ed1ef250c80f0eeaa93b35df2
Zord <quangsang.red@gmail.com>
2026-05-10 22:11:34 +0700

    feat: add CreateRoomButton component

---
+++ webapp/quick-uni/src/components/features/academic/CreateRoomButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { RoomForm } from "./RoomForm";
+
+export function CreateRoomButton({ buildings }: { buildings: any[] }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="gap-2">
+          <Plus className="h-4 w-4" /> Add Room
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Room</DialogTitle>
+        </DialogHeader>
+        <RoomForm buildings={buildings} onSuccess={() => setOpen(false)} />
+      </DialogContent>
+    </Dialog>
+  );
+}
