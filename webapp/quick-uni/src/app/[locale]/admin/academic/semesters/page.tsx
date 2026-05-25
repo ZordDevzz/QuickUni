009d7983418004bf86bf471a9a2722ae1e672607
Zord <quangsang.red@gmail.com>
2026-05-25 12:21:43 +0700

    feat(academic): add semesters management page

--- webapp/quick-uni/src/app/[locale]/admin/academic/semesters/page.tsx
+++ webapp/quick-uni/src/app/[locale]/admin/academic/semesters/page.tsx
@@ -1,5 +1,12 @@
-import AdminSkeleton from "@/components/shared/AdminSkeleton";
+import { getSemesters } from "@/actions/academic";
+import { SemesterClient } from "./semester-client";
 
-export default function SemestersPage() {
-  return <AdminSkeleton title="Semesters" />;
+export default async function SemestersPage() {
+  const data = await getSemesters();
+  
+  return (
+    <div className="flex-1 space-y-4 p-8 pt-6">
+      <SemesterClient data={data} />
+    </div>
+  );
 }
