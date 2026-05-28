e11613286c8075db59705ad8ac82bf5492b46753
Zord <quangsang.red@gmail.com>
2026-05-28 12:30:58 +0700

    .

--- webapp/quick-uni/next.config.ts
+++ webapp/quick-uni/next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["222.255.117.244", "localhost:3000"],
 };
 
 export default withNextIntl(nextConfig);
